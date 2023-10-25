e0acf0f375fea9a32e9ab5b48b1050c6e1a944b2
Tanvir Hossain Sadi <htanvir.sadi@gmail.com>
2023-10-25 11:03:54 +0600

    feat: Displayed All the Folders via API

--- src/components/Folder.tsx
+++ src/components/Folder.tsx
@@ -1,23 +1,38 @@
-import React, {  useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import '../App.css';
+import axios from 'axios';
 import Folder from './FolderStructure';
 
 
 type FolderType = {
     _id: string;
+    parentId?: string;
     name: string;
-    children?: FolderType[] | [];
+    children: FolderType[];
 }
 
 const App: React.FC = () => {
-    const [folders, setFolders] = useState<any>([])
+    const [folders, setFolders] = useState<FolderType[] | []>([])
 
-    console.log(folders);
+    useEffect(() => {
+        const getData = async () => {
+            const data = await axios.get(`http://localhost:500/api/folder/folders`);
+            setFolders([data.data.rootFolders])
+        };
+
+        getData()
+    }, [])
 
     return (
         <div className="folder-structure">
-            {folders.map((folder: FolderType, index: number) => (
-                <Folder key={index} name={folder.name} children={folder.children} />
+            {folders.map((folder: FolderType) => (
+                <Folder
+                    key={folder._id}
+                    _id={folder._id}
+                    parentId={folder.parentId || ""}
+                    name={folder.name}
+                    children={folder.children}
+                />
             ))}
         </div>
     );
--- src/components/FolderStructure.tsx
+++ src/components/FolderStructure.tsx
@@ -3,36 +3,43 @@ import '../App.css';
 import { AddFolderIcon, CloseFolderIcon, DeleteFolderIcon, OpenFolderIcon } from './icons';
 
 interface FolderPropsType {
-    name: string;
-    children?: FolderPropsType[];
+  _id: string;
+  parentId?: string;
+  name: string;
+  children:FolderPropsType[];
 }
 
-const Folder: React.FC<FolderPropsType> = ({ name, children }) => {
+const Folder: React.FC<FolderPropsType> = ({ _id, parentId, name, children }) => {
 
-    return (
-        <div className="folder">
-            <div className="folder-header">
-                <div className='folderIcon'>
-                    {/* <CloseFolderIcon /> */}
-                    <OpenFolderIcon />
-                </div>
-                {name}
-                <div className='addDeleteIcon'>
-                    <AddFolderIcon />
-                    {
-                        name === 'root' || <DeleteFolderIcon />
-                    }
-                </div>
-            </div>
-            {children && children.length > 0 && (
-                <div className="folder-children">
-                    {children.map((child, index) => (
-                        <Folder key={index} name={child.name} children={child.children} />
-                    ))}
-                </div>
-            )}
+ 
+
+  return (
+    <div className="folder">
+      <div className="folder-header">
+        <div className='folderIcon'>
+          {children?.length > 0 ? <OpenFolderIcon /> : <CloseFolderIcon />}
         </div>
-    );
+        {name}
+        <div className='addDeleteIcon'>
+          <AddFolderIcon />
+          {name === 'root' || <DeleteFolderIcon />}
+        </div>
+      </div>
+      {children && children.length > 0 && (
+        <div className="folder-children">
+          {children.map((child) => (
+            <Folder
+              key={child._id}
+              _id={child._id}
+              parentId={child.parentId || ""}
+              name={child.name}
+              children={child.children}
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  );
 };
 
 
